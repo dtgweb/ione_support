824440f055b81c28c7844d99f561e4586d1bbcfc
Brian Johnson <github@brianjohnson.cc>
2009-09-21 20:45:31 -0500

    use all events for ical

--- app/controllers/support_calendar_events_controller.rb
+++ app/controllers/support_calendar_events_controller.rb
@@ -7,7 +7,7 @@ class SupportCalendarEventsController < ApplicationController
     
     response_for :index do |format|
        format.ext { render :layout => false }
-       format.ics { render :text => SupportCalendarEvent.to_ics(current_model.all) }
+       format.ics { render :text => SupportCalendarEvent.to_ics(current_model_without_permission.with_permission(current_user).all) }
        format.html
        format.json do
          render :json => render_json
